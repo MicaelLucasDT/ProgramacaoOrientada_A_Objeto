a42ba9e7986f18de8da12197092140236bd0d73e
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-05-09 11:31:42 -0300

    DISCIPLINA

---
+++ Principal.java
@@ -0,0 +1,47 @@
+package POO.DisciplinaV2;
+
+import java.util.Scanner;
+
+import POO.Disciplina.Disciplina;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Aluno aluno = new Aluno();
+		Disciplina disciplina = new Disciplina();	
+		
+		System.out.println(""); //faculdade, nome idade
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("Quantas disciplinas irá informar? ");
+		int qtdDisciplinas = sc.nextInt();
+		
+		for(int i = 1; i <= qtdDisciplinas; i++) {
+			
+			System.out.println("Infome a disciplina: " + i + " : ");
+			String disc = sc.next();
+			System.out.println("Informe a Nota: " + i + " : ");
+			double nota = sc.nextDouble();
+		    
+			disciplina.setDisciplina(disc);
+			disciplina.setNota(nota);
+			aluno.getDisciplina().add(disciplina);
+		}
+		
+		System.out.println(aluno);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
